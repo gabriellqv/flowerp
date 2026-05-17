7f6964e37183d762558f8ebbd18e9813c2328709
Gabriel Queiroz <gabriellqv@gmail.com>
2026-05-17 00:50:38 -0300

    fix(database): ajusta array filter das opcoes pdo do mysql

--- backend/config/database.php
+++ backend/config/database.php
@@ -60,9 +60,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA', 'fake-path-to-force-ssl'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY', false),
-            ]) : [],
+            ], fn ($value) => ! is_null($value)) : [],
         ],
 
         'mariadb' => [
